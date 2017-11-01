35b648b928bf75f30ed2634a33390144822cebb8
Sailesh <saileshsimhadri@gmail.com>
2017-10-31 21:58:30 -0400

    Update datastructure of animations and implement clear functionality

--- backend/main_parser.py
+++ backend/main_parser.py
@@ -54,13 +54,15 @@ def parse(text):
             arguments = data[1:]
             if element_type == 'step':
                 if(len(curr_step) > 0):
-                    animations.append(curr_step)
+                    animations.append(curr_step[:])
+            elif element_type == 'clear':
                     curr_step = []
             else:
                 obj = parsers[element_type](arguments, object_dict)
                 if obj is not None:
                     for e in obj:
-                        curr_step.append(e.name)
+                        if(e.name not in curr_step):
+                            curr_step.append(e.name)
 
     return create_output(object_dict, text, animations)
 
@@ -102,7 +104,7 @@ def get_text(match):
 def parse_line(args, obj):
     name = ''.join(sorted([x for x in args[0]]))
     point_list = []
-    ret = None
+    ret = []
 
     for p in name:
         if obj.get(p) is None:
@@ -116,10 +118,11 @@ def parse_line(args, obj):
         line = primitives.Line(name)
         line.p1 = point_list[0]
         line.p2 = point_list[1]
-        ret = [line] + point_list
         obj[name] = line
     else:
-        line = None
+        line = obj.get(name);
+
+    ret.extend((line, line.p1, line.p2))
 
     return ret
 
@@ -128,7 +131,7 @@ def parse_circle(args, obj):
     n = ''.join(args)
     name = ''.join(sorted(n))
     point_list = []
-    ret = None
+    ret = []
 
     for p in name:
         if obj.get(p) is None:
@@ -143,10 +146,12 @@ def parse_circle(args, obj):
         circle.p1 = point_list[0]
         circle.p2 = point_list[1]
         circle.p3 = point_list[2]
-        ret = [circle] + point_list
         obj[name] = circle
     else:
-        circle = None
+        circle = obj.get(name)
+
+    ret.append(circle)
+    ret.extend(point_list)
 
     return ret
 
@@ -154,14 +159,15 @@ def parse_circle(args, obj):
 def parse_point(args, obj):
     args = ''.join(args)
     name = args
-    ret = None
+    ret = []
     if obj.get(name) is None:
         point = primitives.Point(name)
         ret = [point]
         obj[name] = point
     else:
-        point = None
+        point = obj.get(name)
 
+    ret.append(point)
     return ret
 
 
@@ -169,10 +175,10 @@ def parse_center(args, obj):
     # ASSUME CIRCLE ALREADY EXISTS
     name = args[0]
     circle = args[1].split("=")[1]
-    ret = None
+    ret = []
 
     if obj.get(name):
-        point = None
+        point = obj.get(name)
     else:
         point = primitives.Point(name=name)
         ret = [point]
@@ -180,6 +186,7 @@ def parse_center(args, obj):
 
     circle = obj[circle]
     circle.center = point
+    ret.append(point)
     return ret
 
 
@@ -187,7 +194,7 @@ def parse_triangle(args, obj):
     n = ''.join(args)
     name = ''.join(sorted(n))
     point_list = []
-    ret = None
+    ret = []
 
     for p in name:
         if obj.get(p) is None:
@@ -203,8 +210,10 @@ def parse_triangle(args, obj):
         ret = [triangle] + point_list
         obj[name] = triangle
     else:
-        triangle = None
+        triangle = obj.get(name)
 
+    ret.append(triangle)
+    ret.extend(point_list)
     return ret
 
 
