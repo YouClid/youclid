16fc99ed5a8aa0b7c91e709935dbc3609a7bc219
Joe Sweeney <joe@joesweeney.org>
2018-01-30 09:21:30 -0500

    Change index to use draw.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,112 +1,52 @@
 
-let scene, camera, raycaster, renderer, mouse, geometry, scenes, anim_index, canvasRect
 
-let size = Math.min(window.innerWidth*0.65, window.innerHeight)
-
-let current = null
-let oldPos = null
-let newPos = null
-let originalCenter = null
-
-
-function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, 1, 1, 1000 );
-    camera.position.set(0, 0, 20 );
-    // camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-    raycaster = new THREE.Raycaster();
-
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(size, size);
-    document.body.appendChild( renderer.domElement );
-
-    canvasRect = renderer.domElement.getBoundingClientRect()
-    
-    renderer.render(scene, camera)
-
-    mouse = new THREE.Vector2();
-
-    anim_index = 0
-    scenes = loadGeometry(camera)
-
-    // document.addEventListener( 'mousedown', onMouseDown, false );
-    document.addEventListener( 'mousemove', onMouseMove, false );
-    // document.addEventListener( 'mouseup', onMouseUp, false );
-    // document.addEventListener( 'touchstart', onTouchStart, false );
-    // document.addEventListener( 'touchmove', onTouchMove, false );
-    // document.addEventListener( 'touchend', onTouchEnd, false );
-    document.addEventListener( 'keydown', onKeyDown)
-
-/*Adds listeners for hovering over text span elements */
-    var mydiv = document.getElementById("text");
-    mydiv.addEventListener("mouseover", overTextChange, false);
-    mydiv.addEventListener("mouseout", overTextRevert, false);
-
-    window.onresize = resize;
-    animate();
-}
-
-function animate() {
-    scene = scenes[anim_index];
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
-
-}
-
-function resize() {
-    size = Math.min(window.innerWidth*0.65, window.innerHeight)
-    renderer.setSize(size, size)
-    canvasRect = renderer.domElement.getBoundingClientRect()
-}
-
-
-function loadGeometry(camera) {
-    let scenes = []
-
-    let steps = geometry.animations.map(arr => arr.map(obj => geometry.geometry[obj]))
-
-    let temp = new THREE.Scene()
-    for(let i = 0; i < steps.length; i++) {
-	let geo = steps[i]
-	scene = temp.clone()
-
-	let points = geo.filter(entry => entry.type === "Point")
-
-	let pointMap = {}
-	for(let i = 0; i < points.length; i++) {
-	    let point = points[i]
-	    pointMap[point.id] = point.data
-	    let p = makePoint(point.id, point.data)
-	    // pointMap[point.id] = p.geometry.vertices[0]
-	    scene.add(p)
+function makeRender(geometry, step) {
+    let toDraw = geometry.animations[step]
+    let objects = geometry.geometry
+    for(let key in objects) {
+	let obj = objects[key]
+	if(obj.type === "Circle") {
+	    if(!obj.data.center || !obj.data.radius) {
+		let circle = circleFromPoints(objects[obj.data.p1].data,
+					      objects[obj.data.p2].data,
+					      objects[obj.data.p3].data)
+		obj.data.center = circle.center
+		obj.data.radius = circle.radius
+	    }
+	    else {
+		obj.data.center = objects[obj.data.center].data
+	    }
 	}
-
-	let circles = geo.filter(entry => entry.type === "Circle")
-
-	for(let i = 0; i < circles.length; i++) {
-	    let circle = circles[i]
-
-	    let d = circle.data
-
-	    let c = makeCircle(circle.id, pointMap[d.p1], pointMap[d.p2], pointMap[d.p3])
-	    scene.add(c)
-	}
-
-	let lines = geo.filter(entry => entry.type === "Line")
-
-	for(let i = 0; i< lines.length; i++) {
-	    let line = lines[i]
-	    let d = line.data
-
-	    let l = makeLine(line.id, pointMap[d.p1], pointMap[d.p2], true)
-
-	    scene.add(l)
-	}
-	scenes.push(scene)
     }
+    
+    return function(visual) {
+	for(let i = 0; i < toDraw.length; i++) {
+	    let id = toDraw[i]
+	    let geo = objects[id]
+	    let red = [1.0, 0.0, 0.0, 1.0]
 
-    return scenes
+	    switch(geo.type) {
+	    case "Point":
+		visual.drawPoint(geo.id, geo.data, red)
+		break;
+	    case "Line":
+		visual.drawLine(geo.id,
+				objects[geo.data.p1].data,
+				objects[geo.data.p2].data,
+				red)
+		break;
+	    case "Circle":
+		visual.drawCircle(geo.id, geo.data.center, geo.data.radius, red)
+		break;
+	    case "Polygon":
+		let points = 
+		visual.drawPolygon(geo.id, )
+		break;
+	    default:
+		console.log("We don't handle type " + geo.type)
+	    }
+	}
+    }
 }
 
 function dist(p1, p2) {
@@ -121,31 +61,7 @@ function dist(p1, p2) {
     }
 }
 
-function makeLine(ident, p1, p2, isNDC) {
-    let wp1, wp2
-    if(isNDC) {
-	wp1 = NDCtoWorld(p1.x, p1.y, camera)
-	wp2 = NDCtoWorld(p2.x, p2.y, camera)
-    } else {
-	wp1 = p1, wp2 = p2
-    }
-
-    let g = new THREE.Geometry()
-    g.vertices.push(wp1, wp2)
-    let m = new THREE.MeshBasicMaterial( { color: 0x990000 } )
-
-    let lineret  = new THREE.Line(g,m)
-    let namestart = "object_line_";
-    lineret.name = namestart.concat(ident.toString());
-    return lineret;
-}
-
-function makeCircle(ident, p1, p2, p3, isWorld) {
-    if(!isWorld) {
-	p1 = NDCtoWorld(p1.x, p1.y, camera)
-	p2 = NDCtoWorld(p2.x, p2.y, camera)
-	p3 = NDCtoWorld(p3.x, p3.y, camera)
-    }
+function circleFromPoints(p1, p2, p3) {
     let m1 = (p1.y-p2.y)/(p1.x-p2.x)
     let m2 = (p3.y-p2.y)/(p3.x-p2.x)
 
@@ -157,70 +73,15 @@ function makeCircle(ident, p1, p2, p3, isWorld) {
     let xb = 0.5*(p2.x+p3.x)
     let yb = 0.5*(p2.y+p3.y)
 
-    let center = new THREE.Vector3()
-
+    let center = {x:0, y:0}
     center.x = ((ma*xa)-(mb*xb)+yb-ya)/(ma-mb)
     center.y = (mb*(center.x-xb))+yb
-    center.z = 0
 
-    let worldCenter, worldPoint
+    let radius = dist(center, p1)
 
-
-    worldCenter = center
-    worldPoint = p1
-    // worldCenter = NDCtoWorld(center.x, center.y, camera)
-    // worldPoint  = NDCtoWorld(p1.x, p1.y, camera)
-
-
-    let radius = dist(worldCenter, worldPoint)
-
-    let circleMaterial = new THREE.MeshBasicMaterial( { color: 0x009900 } );
-    let circleGeom = new THREE.CircleGeometry(radius, 100);
-    circleGeom.vertices.shift()
-    circleGeom.vertices.push(circleGeom.vertices[0])
-    var circle = new THREE.Line( circleGeom, circleMaterial );
-    circle.position.set(worldCenter.x, worldCenter.y, worldCenter.z)
-
-    let namestart = "object_circle_";
-    circle.name = namestart.concat(ident.toString());
-
-    return circle
-
-
+    return {center: center, radius: radius}
 }
-
-
-// NDC stands for normalized device coordinates
-function NDCtoWorld(x, y, camera) {
-    let ndcVec = new THREE.Vector2(x, y)
-    let plane = new THREE.Plane(new THREE.Vector3(0,0,1))
-    let raycaster = new THREE.Raycaster()
-    raycaster.setFromCamera( ndcVec, camera )
-    let pos = raycaster.ray.intersectPlane(plane)
-    return pos
-}
-
-
-function makePoint(ident, point) {
-    // Point has an x and a y attribute in NDC coordinates
-    let PARTICLE_SIZE = 0.5
-
-    let vertex = NDCtoWorld(point.x, point.y, camera)
-
-    let geometry = new THREE.Geometry();
-    geometry.vertices.push(vertex)
-    let material = new THREE.PointsMaterial( { size: PARTICLE_SIZE, color: 0x888888 } )
-
-    let particle = new THREE.Points( geometry, material );
-    particle.material.depthTest = false
-    particle.renderOrder = 1000
-
-    let namestart = "object_point_";
-    particle.name = namestart.concat(ident.toString());
-
-    return particle
-}
-
+    
 
 
 /*
