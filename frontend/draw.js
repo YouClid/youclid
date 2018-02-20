4eaa990a8b60213ed4d82bf1e6edb756f260ca28
Joe Sweeney <joe@joesweeney.org>
2018-02-20 08:50:04 -0500

    Switch lines to rectangle, broke circles
    In the process of changing circles to using small rectangles instead of
    lines.

--- frontend/draw.js
+++ frontend/draw.js
@@ -35,6 +35,7 @@ class Visual {
 	this.canvasRect = null
 
 	this.mouseDown = false
+	this.lineWidth = 0.01
 	
 	this.render = renderFunc
 
@@ -202,6 +203,37 @@ class Visual {
     }
 
 
+    makeLineBuf(points, color, width) {
+
+	let vertices = []
+
+	for(let i=0; i<points.length; i++) {
+	    let p1 = points[i]
+	    let p2 = i < points.length - 1 ? points[i+1] : points[i-1]
+	    let n = normal(sub(p1, p2))
+	
+	    let v1 = [
+		p1.x + (width / 2 * n.x),
+		p1.y + (width / 2 * n.y),
+		0.0,
+		1.0
+	    ]
+	    v1 = v1.concat(color)
+	
+	    let v2 = [
+		p1.x - (width / 2 * n.x),
+		p1.y - (width / 2 * n.y),
+		0.0,
+		1.0
+	    ]
+	    v2 = v2.concat(color)
+
+	    vertices = vertices.concat(v1).concat(v2)
+	}
+
+	return vertices
+    }
+
     drawLine(ident, p1, p2, color) {
 
 	let name = "object_line_" + ident.toString()
@@ -227,16 +259,8 @@ class Visual {
 
 	let FSIZE = data.BYTES_PER_ELEMENT
 
-	let v1 = [
-	    p1.x, p1.y, 0.0, 1.0
-	]
-	v1 = v1.concat(color)
-	let v2 = [
-	    p2.x, p2.y, 0.0, 1.0
-	]
-	v2 = v2.concat(color)
-
-	let vertices = v1.concat(v2)
+	let width = this.lineWidth
+	let vertices = this.makeLineBuf([p1, p2], color, width)	
     
 	data.set(vertices)
 
@@ -249,7 +273,7 @@ class Visual {
 	gl.enableVertexAttribArray(a_Color)
 	gl.vertexAttribPointer(a_Color, 4, gl.FLOAT, false, 8*FSIZE, 4*FSIZE)
 
-	gl.drawArrays(gl.LINES, 0, 2)
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
 
 	return hot
 
@@ -303,11 +327,8 @@ class Visual {
 
 	let FSIZE = data.BYTES_PER_ELEMENT
 
-	let vertices = points.map((p) => {
-	    return [p.x, p.y, 0.0, 1.0].concat(color)
-	})
-	
-	data.set(this.flatten(vertices))
+	let vertices = this.makeLineBuf(points, color, this.lineWidth)
+	console.log(vertices)
 
 	gl.bufferData(gl.ARRAY_BUFFER, data, gl.DYNAMIC_DRAW)
 	let a_Position = gl.getAttribLocation(gl.program, "a_Position")
@@ -318,10 +339,10 @@ class Visual {
 	gl.enableVertexAttribArray(a_Color)
 	gl.vertexAttribPointer(a_Color, 4, gl.FLOAT, false, 8*FSIZE, 4*FSIZE)
 
-    if (fill === true)
-        gl.drawArrays(gl.TRIANGLE_FAN, 0, points.length+1)
-    else
-        gl.drawArrays(gl.LINE_STRIP, 0, points.length+1)
+	if (fill === true)
+            gl.drawArrays(gl.TRIANGLE_FAN, 0, points.length+1)
+	else
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, points.length+1)
 
 	return hot
 
@@ -454,6 +475,15 @@ function norm(v) {
 
 function scale(v, c) {
     return v2(v.x*c, v.y*c)
+}
+
+function normal(v) {
+    // Return the unit vector normal to v
+    let n = norm(v)
+    if(n == 0.0)
+	return v2(0,0)
+    v = scale(v, 1.0/norm(v))
+    return v2(v.y, -v.x)
 }
 
 
