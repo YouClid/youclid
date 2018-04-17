d76dcea265ea4f9df8328e989a286e528e4b9ddf
Drew Monroe <d.monroe@snet.net>
2018-04-17 11:15:12 -0400

    Add proposition 6 example

---
+++ frontend/examples/proposition6/draw.js
@@ -0,0 +1,618 @@
+const vertexShader = `
+precision mediump float;
+attribute vec4 a_Position;
+attribute vec4 a_Color;
+varying vec4 v_Color;
+void main() {
+     gl_Position = a_Position;
+     gl_PointSize = 10.0;
+     v_Color = a_Color;
+}
+`
+
+const fragmentShader = `
+precision mediump float;
+varying vec4 v_Color;
+void main() {
+     gl_FragColor = v_Color;
+}
+`
+
+
+class Visual {
+    constructor() {
+	this.mouse = {x:1000, y:10000}
+	this.mousedown = false
+	this.mouseup = true
+
+	this.gl = null
+	this.glData = null
+
+	this.drawn = {}
+
+	this.size = Math.min(window.innerWidth*0.65, window.innerHeight)
+
+	this.canvasRect = null
+
+	this.mouseDown = false
+	this.lineWidth = 0.01
+
+	this.light = false
+	this.hotColor = [1.0, 1.0, 0, 1.0]
+	
+	this.init()
+    }
+    
+    init() {
+	
+	
+	let canvas = document.createElement('canvas')
+	document.body.appendChild( canvas )
+
+	// Adjust canvas size based on screen
+	{
+	    canvas.style.width = this.size + "px";
+	    canvas.style.height = this.size + "px";
+
+	    // set the size of the drawingBuffer
+	    var devicePixelRatio = window.devicePixelRatio || 1;
+	    canvas.width = this.size * devicePixelRatio;
+	    canvas.height = this.size * devicePixelRatio;
+	}
+
+	let gl = canvas.getContext('webgl')
+	
+	let program = this.initShaders(gl, vertexShader, fragmentShader)
+	gl.program = program
+	gl.useProgram(program)
+	
+	let buf = gl.createBuffer()
+	gl.bindBuffer(gl.ARRAY_BUFFER, buf)
+	this.glData = new Float32Array(1024)
+
+	this.gl = gl
+	this.canvasRect = canvas.getBoundingClientRect()
+
+	gl.lineWidth(4.0)
+
+	// Register listeners
+	document.addEventListener( 'mousemove', onMouseMove.bind(this));
+	document.addEventListener( 'mousedown', () => this.mouseDown = true)
+	document.addEventListener( 'mouseup', () => this.mouseDown = false)
+	window.addEventListener( 'resize', onResize.bind(this));
+
+    }
+
+    clear() {
+	let gl = this.gl
+	if(this.light){
+	    let bg = 1.0
+	    gl.clearColor(bg, bg, bg, 1.0)
+	    this.hotColor = [0.9, 0.9, 0.2, 1.0]
+	}
+	else {
+	    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+	    this.hotColor = [1.0, 1.0, 0, 1.0]
+	}
+	gl.clear(gl.COLOR_BUFFER_BIT)
+    }
+    
+    isHot(name) {
+	let hot = this.hot[name]
+	return hot ? true : false
+    }
+
+    initShaders(gl, vertSrc, fragSrc) {
+	let vshader = gl.createShader(gl.VERTEX_SHADER)
+	let fshader = gl.createShader(gl.FRAGMENT_SHADER)
+
+	gl.shaderSource(vshader, vertSrc)
+	gl.shaderSource(fshader, fragSrc)
+
+	gl.compileShader(vshader)
+	gl.compileShader(fshader)
+
+
+	if (!gl.getShaderParameter(vshader, gl.COMPILE_STATUS)) {
+	    alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(vshader));
+	    return null;
+	}
+	if (!gl.getShaderParameter(fshader, gl.COMPILE_STATUS)) {
+	    alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(fshader));
+	    return null;
+	}
+
+
+	const shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vshader);
+	gl.attachShader(shaderProgram, fshader);
+	gl.linkProgram(shaderProgram);
+	
+	var success = gl.getProgramParameter(shaderProgram, gl.LINK_STATUS);
+	if (!success) {
+	    // something went wrong with the link
+	    throw ("program filed to link:" + gl.getProgramInfoLog (shaderProgram));
+	}
+	
+	return shaderProgram
+    }
+
+
+    dist(p1, p2) {
+	if(p1.z && p2.z) {
+	    let xs, ys, zs
+	    xs = (p1.x-p2.x)*(p1.x-p2.x)
+	    ys = (p1.y-p2.y)*(p1.y-p2.y)
+	    zs = (p1.z-p2.z)*(p1.z-p2.z)
+	    return Math.sqrt(xs+ys+zs)
+	} else {
+	    return Math.sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y))
+	}
+    }
+
+    pointUnderMouse(point) {
+	let sz = 5.0/(2*this.size)
+	let center = {x:point.x+(sz/2), y:point.y+(sz/2)}
+	return this.dist(this.mouse, center) < 0.05
+    }
+
+    lineUnderMouse(p0, p1) {
+	let mouse = this.mouse
+	let n = sub(p1, p0)
+	let a = p0
+	let a_to_m = sub(mouse, a)
+	let coeff = dot(n, a_to_m)/normsq(n)
+	if(coeff < 0 || coeff > 1) {
+	    return false
+	}
+	let d = this.dist(a_to_m, scale(n, coeff))
+	return d < 0.02
+    }
+
+    polyUnderMouse(points) {
+	if(points.length < 2) {
+	    return false
+	}
+	for(let i = 0; i<points.length; i++) {
+	    let next = (i+1)%points.length
+	    let p0 = points[i]
+	    let p1 = points[next]
+	    if(this.lineUnderMouse(p0, p1)) {
+		return true
+	    }
+	}
+	return false
+    }
+
+
+
+    flatten(arr) {
+	return [].concat.apply([], arr);
+    }
+
+    /*************************************
+
+         Drawing Primitives
+
+    *************************************/
+
+
+    drawPoint(ident, point, color) {
+        // create a circular point (filled in) of radius 0.0125
+	if(this.pointUnderMouse(point)) {
+	    color = this.hotColor
+	}
+	let strokeColor = [0,0,0,1]
+        return this.drawCircle(ident, point, 0.013, strokeColor, true, color, 0.001)
+    }
+
+
+    makeLineBuf(points, color, width) {
+
+	let vertices = []
+
+	for(let i=0; i<points.length; i++) {
+	    let p1 = points[i]
+	    let p2 = i < points.length - 1 ? points[i+1] : points[i-1]
+	    let n = i < points.length - 1 ? normal(sub(p1, p2)) : normal(sub(p2, p1))
+	
+	    let v1 = [
+		p1.x + (width / 2 * n.x),
+		p1.y + (width / 2 * n.y),
+		0.0,
+		1.0
+	    ]
+	    v1 = v1.concat(color)
+	
+	    let v2 = [
+		p1.x - (width / 2 * n.x),
+		p1.y - (width / 2 * n.y),
+		0.0,
+		1.0
+	    ]
+	    v2 = v2.concat(color)
+
+	    vertices = vertices.concat(v1).concat(v2)
+	}
+
+	return vertices
+    }
+
+    drawLine(ident, p1, p2, color, changeOnHot=true) {
+
+	let name = "object_line_" + ident.toString()
+
+	let hot = false
+	let active = false
+    
+	if(this.lineUnderMouse(p1, p2)) {
+	    hot = true
+	    if(this.mouseDown) {
+		active = true
+	    }
+	}
+    
+	if(hot && changeOnHot) {
+	    color = this.hotColor
+	}
+
+    
+	let gl = this.gl
+    
+	let data = this.glData
+
+	let FSIZE = data.BYTES_PER_ELEMENT
+
+	let width = this.lineWidth
+	let vertices = this.makeLineBuf([p1, p2], color, width)	
+    
+	data.set(vertices)
+
+	gl.bufferData(gl.ARRAY_BUFFER, data, gl.DYNAMIC_DRAW)
+	let a_Position = gl.getAttribLocation(gl.program, "a_Position")
+	gl.enableVertexAttribArray(a_Position)
+	gl.vertexAttribPointer(a_Position, 4, gl.FLOAT, false, 8*FSIZE, 0)
+
+	let a_Color = gl.getAttribLocation(gl.program, "a_Color")
+	gl.enableVertexAttribArray(a_Color)
+	gl.vertexAttribPointer(a_Color, 4, gl.FLOAT, false, 8*FSIZE, 4*FSIZE)
+
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
+
+	return hot
+
+    }
+
+    getPoints(center, radius) {
+	let points = []
+	let theta = 0
+	let inc = 0.05
+	let tau = 2*Math.PI
+	while(theta <= tau) {
+	    let x = radius*Math.cos(theta)
+	    let y = radius*Math.sin(theta)
+	    points.push(add(v2(x, y), center))
+	    theta += inc
+	}
+	let x = radius*Math.cos(0)
+	let y = radius*Math.sin(0)
+	points.push(add(v2(x, y), center))
+	return points
+    }
+
+    drawCircle(ident, center, radius, strokeColor, fill=false, fillColor=[0,0,0,1], strokeWidth=this.lineWidth) {
+
+	let name = "object_line_" + ident.toString()
+
+	let hot = false
+	let active = false
+	
+	let points = this.getPoints(center, radius)
+	
+	if(this.polyUnderMouse(points)) {
+	    hot = true
+	    if(this.mouseDown) {
+		active = true
+	    }
+	} 
+	
+	if(hot) {
+	    // color = this.hotColor
+	    fill = true
+	}
+
+	
+	let gl = this.gl
+
+	let vertices = []
+	let num_indices = 0
+
+
+
+	if(fill) {
+	    vertices = points.map((p) => {
+		return [p.x, p.y, 0.0, 1.0].concat(fillColor)
+	    })
+	    vertices = this.flatten(vertices)
+	    num_indices = points.length + 1
+
+	    if(this.glData.length < vertices.length) {
+		this.glData = new Float32Array(vertices.length)
+	    }
+
+	    let data = this.glData
+	    let FSIZE = data.BYTES_PER_ELEMENT
+
+	    data.set(vertices)
+
+	    gl.bufferData(gl.ARRAY_BUFFER, data, gl.DYNAMIC_DRAW)
+	    let a_Position = gl.getAttribLocation(gl.program, "a_Position")
+	    gl.enableVertexAttribArray(a_Position)
+	    gl.vertexAttribPointer(a_Position, 4, gl.FLOAT, false, 8*FSIZE, 0)
+
+	    let a_Color = gl.getAttribLocation(gl.program, "a_Color")
+	    gl.enableVertexAttribArray(a_Color)
+	    gl.vertexAttribPointer(a_Color, 4, gl.FLOAT, false, 8*FSIZE, 4*FSIZE)
+
+
+	    gl.drawArrays(gl.TRIANGLE_FAN, 0, num_indices)
+	}
+
+	vertices = this.makeLineBuf(points, strokeColor, strokeWidth)
+	num_indices = 2*(points.length)
+
+	if(this.glData.length < vertices.length) {
+	    this.glData = new Float32Array(vertices.length)
+	}
+
+	let data = this.glData
+	let FSIZE = data.BYTES_PER_ELEMENT
+
+	data.set(vertices)
+
+	gl.bufferData(gl.ARRAY_BUFFER, data, gl.DYNAMIC_DRAW)
+	let a_Position = gl.getAttribLocation(gl.program, "a_Position")
+	gl.enableVertexAttribArray(a_Position)
+	gl.vertexAttribPointer(a_Position, 4, gl.FLOAT, false, 8*FSIZE, 0)
+
+	let a_Color = gl.getAttribLocation(gl.program, "a_Color")
+	gl.enableVertexAttribArray(a_Color)
+	gl.vertexAttribPointer(a_Color, 4, gl.FLOAT, false, 8*FSIZE, 4*FSIZE)
+
+
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, num_indices)
+
+	return hot
+
+    }
+
+
+    drawPoly(ident, points, color, fill=false) {
+
+	let name = "object_line_" + ident.toString()
+
+	let hot = false
+	let active = false
+	
+	if(this.polyUnderMouse(points)) {
+	    hot = true
+	    if(this.mouseDown) {
+		active = true
+	    }
+	}
+	
+	if(hot) {
+	    // color = this.hotColor
+	    fill = true
+	}
+
+	points.push(points[0])
+	let vertices = []
+	let num_indices = 0
+
+	if(fill) {
+	    let gl = this.gl
+	    vertices = points.map((p) => {
+		return [p.x, p.y, 0.0, 1.0].concat(color)
+	    })
+	    vertices = this.flatten(vertices)
+	    num_indices = points.length
+	    
+	    if(this.glData.length < vertices.length) {
+		this.glData = new Float32Array(vertices.length)
+	    }
+	    
+	    let data = this.glData
+
+	    let FSIZE = data.BYTES_PER_ELEMENT
+
+
+	    data.set(vertices)
+
+	    gl.bufferData(gl.ARRAY_BUFFER, data, gl.DYNAMIC_DRAW)
+	    let a_Position = gl.getAttribLocation(gl.program, "a_Position")
+	    gl.enableVertexAttribArray(a_Position)
+	    gl.vertexAttribPointer(a_Position, 4, gl.FLOAT, false, 8*FSIZE, 0)
+
+	    let a_Color = gl.getAttribLocation(gl.program, "a_Color")
+	    gl.enableVertexAttribArray(a_Color)
+	    gl.vertexAttribPointer(a_Color, 4, gl.FLOAT, false, 8*FSIZE, 4*FSIZE)
+
+
+	    gl.drawArrays(gl.TRIANGLE_STRIP, 0, num_indices)
+	}
+
+	for(let i = 0; i<points.length-1; i++) {
+	    let p1 = points[i]
+	    let p2 = points[i+1]
+	    this.drawLine(ident, p1, p2, color, false)
+	}
+	
+	return hot
+    }
+
+    getArcPoints(p1, p0, angle) {
+	let d = dist(p0, p1)
+	let radius = d / 4.0
+	let start = scale(sub(p0, p1), radius/d)
+
+	let points = [p1]
+	let theta = 0
+	let inc = 0.02
+	while(theta <= angle) {
+	    let x = (start.x * Math.cos(theta)) + (start.y * -1 * Math.sin(theta))
+	    let y = (start.x * Math.sin(theta)) + (start.y * Math.cos(theta))
+	    points.push(add(v2(x, y), p1))
+	    theta += inc
+	}
+	return points
+    }
+
+    drawAngle(ident, points, color, angle, fill=false) {
+	angle = angle * (Math.PI/180.0)
+
+	let hot = false
+	let active = false
+
+	hot = hot || this.lineUnderMouse(points[0], points[1])
+	hot = hot || this.lineUnderMouse(points[1], points[2])
+	
+	if(hot) {
+	    // color = this.hotColor
+	    fill = true
+	}
+
+	let vertices = []
+	let num_indices = 0
+
+	if(fill) {
+	    let arc = this.getArcPoints(points[1], points[2], angle)
+	    let gl = this.gl
+	    vertices = arc.map((p) => {
+		return [p.x, p.y, 0.0, 1.0].concat(color)
+	    })
+	    vertices = this.flatten(vertices)
+	    num_indices = arc.length
+	    
+	    if(this.glData.length < vertices.length) {
+		this.glData = new Float32Array(vertices.length)
+	    }
+	    
+	    let data = this.glData
+
+	    let FSIZE = data.BYTES_PER_ELEMENT
+
+
+	    data.set(vertices)
+
+	    gl.bufferData(gl.ARRAY_BUFFER, data, gl.DYNAMIC_DRAW)
+	    let a_Position = gl.getAttribLocation(gl.program, "a_Position")
+	    gl.enableVertexAttribArray(a_Position)
+	    gl.vertexAttribPointer(a_Position, 4, gl.FLOAT, false, 8*FSIZE, 0)
+
+	    let a_Color = gl.getAttribLocation(gl.program, "a_Color")
+	    gl.enableVertexAttribArray(a_Color)
+	    gl.vertexAttribPointer(a_Color, 4, gl.FLOAT, false, 8*FSIZE, 4*FSIZE)
+
+
+	    gl.drawArrays(gl.TRIANGLE_FAN, 0, num_indices)
+	}
+
+	for(let i = 0; i<points.length-1; i++) {
+	    let p1 = points[i]
+	    let p2 = points[i+1]
+	    this.drawLine(ident, p1, p2, color, false)
+	}
+	
+	return hot
+    }
+
+
+
+}
+
+
+/*************************************
+
+           Event Listeners
+
+*************************************/
+
+function onMouseMove( event ) {
+    let canvasRect = this.canvasRect
+    let size = this.size
+    
+    let xgood = event.clientX > canvasRect.left && event.clientX < canvasRect.right
+    let ygood = event.clientY > canvasRect.top  && event.clientY < canvasRect.bottom
+
+    if(xgood && ygood) {
+	this.mouseInCanvas = true
+	this.mouse.x = ((event.clientX - canvasRect.left) / size ) * 2 - 1;
+	this.mouse.y = - ( (event.clientY - canvasRect.top) / size ) * 2 + 1;
+    }
+    else {
+	this.mouseInCanvas = false
+    }
+}
+
+function onResize( event ) {
+    this.canvasRect = this.gl.canvas.getBoundingClientRect()
+    this.size = Math.min(window.innerWidth*0.65, window.innerHeight)
+    let realToCSSPixels = window.devicePixelRatio
+    let drawSize = Math.floor(this.size * realToCSSPixels)
+    this.gl.canvas.style.width = this.size + "px";
+    this.gl.canvas.style.height = this.size + "px"; 
+}
+
+/*************************************
+
+        Geometric Predicates
+
+*************************************/
+
+
+function v2(x, y) {
+    return {x:x, y:y}
+}
+
+function dot(v1, v2) {
+    return (v1.x*v2.x)+(v1.y*v2.y)
+}
+
+function add(v1, v2) {
+    return {
+	x: v1.x+v2.x,
+	y: v1.y+v2.y
+    }
+}
+
+function sub(v1, v2) {
+    return {
+	x: v1.x-v2.x,
+	y: v1.y-v2.y
+    }
+}
+
+function normsq(v) {
+    return dot(v,v)
+}
+
+function norm(v) {
+    return Math.sqrt(normsq(v))
+}
+
+function scale(v, c) {
+    return v2(v.x*c, v.y*c)
+}
+
+function normal(v) {
+    // Return the unit vector normal to v
+    let n = norm(v)
+    if(n == 0.0)
+	return v2(0,0)
+    v = scale(v, 1.0/norm(v))
+    return v2(v.y, -v.x)
+}
+
+
+
---
+++ frontend/examples/proposition6/index.js
@@ -0,0 +1,465 @@
+let visual = null
+let hotText = {}
+let labels = {}
+let darkurl = ""
+let lighturl = ""
+
+function init() {
+    visual = new Visual()
+    let renderer = new Renderer(geometry, visual)
+    
+    labels = makeLabels(geometry)
+
+    colorText(geometry)
+
+    document.addEventListener( 'keydown', onKeyDown(renderer))
+    let textElements = Array.from(document.getElementsByClassName('GeoElement'))
+    textElements.forEach(
+	(el) => {
+	    el.addEventListener('mouseenter', overTextChange(renderer))
+	    el.addEventListener('mouseleave', overTextRevert(renderer))
+	})
+    document.getElementById("themeswitch").onclick = themeSwitch(renderer)
+
+    setStyleURLs()
+    
+    renderer.render()
+}
+
+function sortItems(toDraw, objs) {
+    sorted = []
+    // Gonna do 4 passes, first gets circles and polys,
+    // then everything that isn't a point
+    // then all the items that are 'textHot'
+    // then the points
+
+    toDraw.forEach((id) => {
+	if((objs[id].type == "Polygon" || objs[id].type == "Circle") && isHot(objs[id])) {
+	    sorted.push(id)
+	}
+    })
+
+    toDraw.forEach((id) => {
+	if((objs[id].type == "Polygon" || objs[id].type == "Circle") && !isHot(objs[id])) {
+	    sorted.push(id)
+	}
+    })
+
+    toDraw.forEach((id) => {
+	if(objs[id].type == "Angle" && !isHot(objs[id])) {
+	    sorted.push(id)
+	}
+    })
+    
+    toDraw.forEach((id) => {
+	if(objs[id].type == "Line" && !isHot(objs[id])) {
+	    sorted.push(id)
+	}
+    })
+
+    
+    toDraw.forEach((id) => {
+	if(isHot(objs[id]) && objs[id].type != "Polygon" && objs[id].type != "Circle") {
+	    sorted.push(id)
+	}
+    })
+
+    toDraw.forEach((id) => {
+	if(objs[id].type == "Point") {
+	    sorted.push(id)
+	}
+    })
+
+    
+
+    return sorted
+}
+
+class Renderer {
+    constructor(geometry, visual) {
+	this.geo = geometry
+	this.visual = visual
+	this.anim_index = 0
+	this.makeRenderFuncs()
+	if(geometry.animations.length > 1)
+	    this.createStepButtons()
+
+	window.addEventListener("mousemove", (e) => {
+	    if(this.visual.mouseInCanvas) {
+		this.render()
+	    }
+	})
+	this.updateAnimation(0)
+    }
+
+    makeRenderFuncs() {
+	let visual = this.visual
+	this.renderFuncs = []
+	for(let step = 0; step < this.geo.animations.length; step++) {
+	    let toDraw = geometry.animations[step]
+	    let objects = this.geo.geometry
+	    for(let key in objects) {
+		let obj = objects[key]
+		if(obj.type === "Circle") {
+		    if(!obj.data.center && !obj.data.radius) {
+			let circle = circleFromPoints(objects[obj.data.p1].data,
+						      objects[obj.data.p2].data,
+						      objects[obj.data.p3].data)
+			obj.data.center = circle.center
+			obj.data.radius = circle.radius
+		    }
+		    else if (!obj.data.radius && obj.data.center) {
+			// Get the coordinates of a point on the circle
+			let otherPoint = objects[obj.data.p1].data
+			// Get the coordinates of the center of the circle
+			let center = objects[obj.data.center].data
+			// Compute the distance from the center to a point (the radius)
+			obj.data.radius = dist(center, otherPoint)
+			// Update the center of the circle to be the coordinates of
+			// the center
+			obj.data.center = center
+		    }
+	            // The center is given as a point (something like "A")
+		    else if(typeof(obj.data.center) === 'string') {
+			obj.data.center = objects[obj.data.center].data
+		    }
+		}
+	    }
+	    let render =  function() {
+		visual.clear()
+		let toDrawSorted = sortItems(toDraw, objects)
+		for(let i = 0; i < toDrawSorted.length; i++) {
+		    let id = toDrawSorted[i]
+		    let geo = objects[id]
+		    let textHot = isHot(geo)
+		    let color = textHot ? visual.hotColor : objects[id].color
+		    let hot = false
+
+		    switch(geo.type) {
+		    case "Point":
+			hot = visual.drawPoint(geo.id, geo.data, color)
+			highlightText(geo, hot, textHot, true)
+			break;
+		    case "Line":
+			hot = visual.drawLine(geo.id,
+					      objects[geo.data.p1].data,
+					      objects[geo.data.p2].data,
+					      color)
+
+			highlightText(geo, hot, textHot)
+			break;
+		    case "Circle":
+			hot = visual.drawCircle(geo.id, geo.data.center, geo.data.radius, objects[id].color, textHot, objects[id].color)
+			highlightText(geo, hot, textHot)
+			break;
+		    case "Polygon":
+			hot = visual.drawPoly(geo.id, geo.data.points.map((p) => objects[p].data), objects[id].color, textHot)
+			highlightText(geo, hot, textHot)
+			break;
+		    case "Angle":
+			hot = visual.drawAngle(geo.id,
+					       geo.data.points.map((p) => objects[p].data),
+					       geo.color,
+					       geo.data.degree,
+					       textHot)
+			highlightText(geo, hot, textHot)
+			break;
+		    default:
+			console.log("We don't handle type " + geo.type)
+		    }
+		}
+	    }
+
+	    this.renderFuncs.push(render)
+	}
+    }
+
+    render() {
+	this.renderFuncs[this.anim_index]()
+    }
+
+    createStepButtons() {
+	let visual = this.visual
+	let next = document.createElement('input')
+	let prev = document.createElement('input')
+
+	next.className = 'step_btn'
+	next.id = 'next_step'
+	next.value = 'Next Step'
+	next.type = 'button'
+	prev.className = 'step_btn'
+	prev.id = 'prev_step'
+	prev.value = 'Prev Step'
+	prev.type = 'button'
+	
+	next.onclick = () => {
+	    this.updateAnimation(+1)
+	}
+	prev.onclick = () => {
+	    this.updateAnimation(-1)
+	}
+
+	document.body.append(prev)
+	document.body.append(next)
+
+
+	let moveButtons = () => {
+	    let rect = visual.canvasRect
+	    let pad = 40
+
+	    prev.style.left = rect.left + pad + 'px'
+	    prev.style.top = rect.bottom - prev.clientHeight - pad + 'px'
+	    next.style.left  = rect.right - next.clientWidth - pad + 'px'
+	    next.style.top = rect.bottom - next.clientHeight - pad + 'px'
+	}
+	moveButtons()
+
+	window.addEventListener('resize', moveButtons)
+    }
+
+    updateAnimation(delta) {
+	clearText(this.geo, this.anim_index)
+	
+	if(delta < 0) {
+	    this.anim_index = this.anim_index === 0 ? this.anim_index : this.anim_index + delta
+	}
+	else if(delta > 0) {
+	    this.anim_index = this.anim_index === geometry.animations.length-1 ? this.anim_index : this.anim_index + delta
+	}
+
+	let prev = Array.from(document.getElementsByClassName('step_highlighted'))
+	prev.forEach((el) => el.className = '')
+	
+
+	let par = document.getElementById('step_' + this.anim_index)
+	if(par) {
+	    par.className = 'step_highlighted'
+	}
+
+	this.render()
+    }
+
+
+}
+
+function dist(p1, p2) {
+    if(p1.z && p2.z) {
+	let xs, ys, zs
+	xs = (p1.x-p2.x)*(p1.x-p2.x)
+	ys = (p1.y-p2.y)*(p1.y-p2.y)
+	zs = (p1.z-p2.z)*(p1.z-p2.z)
+	return Math.sqrt(xs+ys+zs)
+    } else {
+	return Math.sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y))
+    }
+}
+
+function circleFromPoints(p1, p2, p3) {
+    let m1 = (p1.y-p2.y)/(p1.x-p2.x)
+    let m2 = (p3.y-p2.y)/(p3.x-p2.x)
+
+    let ma = -1.0*(1.0/m1)
+    let mb = -1.0*(1.0/m2)
+
+    let xa = 0.5*(p1.x+p2.x)
+    let ya = 0.5*(p1.y+p2.y)
+    let xb = 0.5*(p2.x+p3.x)
+    let yb = 0.5*(p2.y+p3.y)
+
+    let center = {x:0, y:0}
+    center.x = ((ma*xa)-(mb*xb)+yb-ya)/(ma-mb)
+    center.y = (mb*(center.x-xb))+yb
+
+    let radius = dist(center, p1)
+
+    return {center: center, radius: radius}
+}
+
+function highlightText(geo, isHot, textHot, isPoint) {
+    isHot = isHot || textHot
+    if(textHot || isPoint) {
+	showLabel(geo.id)
+    }
+    else {
+	hideLabel(geo.id)
+    }
+    let elements = document.getElementsByName("text_"+geo.type.toLowerCase()+"_"+geo.id)
+    elements.forEach((el) => {
+	el.style.color = getHex(geo.color)
+	if(isHot) {
+	    el.classList.add('shadowed')
+	}
+	else {
+	    el.classList.remove('shadowed')
+	}
+    })
+}
+
+function getHex(colorArr) {
+    let color = '#'
+    for(let i = 0; i<colorArr.length-1; i++) {
+	let val =  Math.floor(255*colorArr[i]).toString(16)
+	while(val.length < 2) {
+	    val = '0' + val
+	}
+	color += val
+    }
+    return color
+}
+
+function isHot(geo) {
+    return hotText[geo.type.toLowerCase()+"_"+geo.id] ? true : false
+}
+
+
+function makeLabels(data) {
+    let geometry = data.geometry
+    let anims = data.animations
+    for(let i = 0; i<anims.length; i++) {
+	for(let j = 0; j<anims[i].length; j++) {
+	    let obj = geometry[anims[i][j]]
+	    if(labels[obj.id]) continue
+	    let elem = document.createElement('div')
+	    elem.id = obj.id
+	    elem.innerHTML = obj.id
+	    elem.style.position = 'absolute'
+	    elem.className = 'label'
+	    elem.style.display = "none"
+	    let x = 0
+	    let y = 0
+	    let point = null
+	    switch(obj.type) {
+	    case "Point":
+		x = obj.data.x
+		y = obj.data.y
+		break;
+	    // case "Line":
+	    // 	point = geometry[obj.data.p2].data
+	    // 	x = point.x
+	    // 	y = point.y
+	    // 	break;
+	    // case "Circle":
+	    // 	x = obj.data.center.x
+	    // 	y = obj.data.center.y
+	    // 	break;
+	    // case "Polygon":
+	    // 	point = geometry[obj.data.points[0]].data
+	    // 	x = point.x
+	    // 	y = point.y
+	    // 	break;
+	    default:
+		continue
+	    }
+	    x = ( x + 1) / 2 + 0.012
+	    y = (-y + 1) / 2 - 0.03
+	    elem.style.left = (Math.floor(x * visual.size) + visual.canvasRect.left) + 'px'
+	    elem.style.top = (Math.floor(y * visual.size) + visual.canvasRect.top) + 'px'
+	    labels[obj.id] = elem
+	    document.body.appendChild(elem)
+	}
+    }
+}
+
+function showLabel(id) {
+    let el = document.getElementById(id)
+    if(el) el.style.display = "initial"
+}
+
+function hideLabel(id) {
+    let el = document.getElementById(id)
+    if(el) el.style.display = "none"
+}
+
+function clearText(geometry, step) {
+    geometry.animations[step].forEach((id) => {
+	highlightText(geometry.geometry[id], false, false)
+    })
+}
+
+function colorText(geometry) {
+    geometry.animations.forEach((step) => {
+	step.forEach((id) => highlightText(geometry.geometry[id], false, false))
+    })
+}
+
+
+
+
+/*
+  All Event Callbacks
+*/
+
+/* Two functions for hovering over span elements, and leaving from hover */
+function overTextChange(renderer)  {
+    return (event) => {
+	let obj_id_str = event.target.getAttribute("name").replace('text_', '');
+	hotText[obj_id_str] = true
+	renderer.render()
+    }
+}
+
+function overTextRevert(renderer)  {
+    return () => {
+	let obj_id_str = event.target.getAttribute("name").replace('text_', '');
+	hotText[obj_id_str] = false
+	renderer.render()
+    }
+}
+
+
+function onTouchStart( event ) {
+    event.preventDefault();
+    event.clientX = event.touches[0].clientX;
+    event.clientY = event.touches[0].clientY;
+    onMouseDown( event );
+}
+
+function onTouchMove( event ) {
+    event.preventDefault();
+    event.clientX = event.touches[0].clientX;
+    event.clientY = event.touches[0].clientY;
+    onMouseMove( event );
+}
+
+function onTouchEnd( event ) {
+    event.preventDefault();
+    onMouseUp( event );
+}
+
+function onKeyDown( r ) {
+    return () => {
+	if(event.keyCode == 37) {
+	    r.updateAnimation(-1)
+	}
+	else if(event.keyCode == 39) {
+	    r.updateAnimation(+1)
+	}
+    }
+}
+
+function themeSwitch(renderer) {
+    return () => {
+	let style = document.getElementById("stylesheet")
+	let check = document.getElementById("themeswitch")
+
+	if(check.checked) {
+	    style.href = lighturl
+	    visual.light = true
+	}
+	else {
+	    style.href = darkurl
+	    visual.light = false
+	}
+	renderer.render()
+    }
+}
+
+function setStyleURLs() {
+    let style = document.getElementById("stylesheet")
+
+    darkurl = style.href
+    lighturl = darkurl.replace("default.css", "light.css")
+    console.log(darkurl)
+    console.log(lighturl)
+}
+
